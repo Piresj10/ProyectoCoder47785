9a82d42618dbc28232044d09b1949506d66f40d8
Javier Pires <javierpires30@gmail.com>
2023-11-27 21:45:52 -0300

    usando plantillas

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -9,5 +9,11 @@ def crear_curso(request):
     curso = Curso(nombre="Python", camada=47785)
 
     curso.save()
+    contexto = {"curso": curso}
 
-    return HttpResponse(f"su curso es {curso.nombre} y la camada es {curso.camada}")
+    return render(request, 'index.html', contexto)
+
+def show_html(request):
+    curso = Curso.objects.first()
+    contexto = {"curso": curso, "nombre": "Javier"}
+    return render(request, 'index.html', contexto)
