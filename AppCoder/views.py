a1aa3d9742e5cd5f394197aeced6b3a2864549b0
Javier Pires <javierpires30@gmail.com>
2023-11-28 22:41:17 -0300

    herencia de plantillas

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,17 +1,25 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from AppCoder.models import Curso
 
 
 
 # Create your views here.
+
+def mostrar_cursos(request):
+    cursos = Curso.objects.all()
+    contexto = {
+        "cursos": cursos,
+        "nombre": "Login"
+    }
+    return render(request, "AppCoder/cursos.html", contexto)
 def crear_curso(request):
-    curso = Curso(nombre="Python", camada=47785)
+    curso = Curso(nombre="Python", camada=47783)
 
     curso.save()
     contexto = {"curso": curso}
 
-    return render(request, 'index.html', contexto)
+    return redirect("/app/cursos/")
 
 def show_html(request):
     curso = Curso.objects.first()
